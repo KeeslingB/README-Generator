5dc5d78ac86500efc912057aaac8a410db8e9899
unknown <Breke64@gmail.com>
2023-10-27 16:50:07 -0400

    final commit to repo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const inquirer = require('inquirer');
-
+const ignore = `node_modules`;
 
 inquirer.prompt([
   {
@@ -17,9 +17,9 @@ inquirer.prompt([
     type: 'checkbox',
     message: 'What sections would you like included in your README(Sellect all that apply)',
     name: 'sections',
-    choices: (['Installation instructions','Usage information','Contributions guidelines','Test instructions']),
+    choices: ['Installation instructions', 'Usage information', 'Contributions guidelines', 'Test instructions'],
     validate: (choices) => {
-      if(!choices.length){
+      if (!choices.length) {
         return 'atleast one selection please'
       }
       return true;
@@ -29,12 +29,12 @@ inquirer.prompt([
     type: 'list',
     message: 'What Licenses would you like?',
     name: 'license',
-    choices: ['MIT','Mozilla','Perl','Apache']
+    choices: ['MIT', 'Mozilla', 'Perl', 'Apache']
   },
   {
     type: 'confirm',
     name: 'gitignore',
-    message: 'Would you like a .gitignore file included?'  
+    message: 'Would you like a .gitignore file included?'
   },
   {
     type: 'input',
@@ -47,23 +47,29 @@ inquirer.prompt([
     name: 'email'
   },
 ])
-.then((response)=>
-createPage(response));
+  .then((response) =>
+    createPage(response));
 
+    
+function createPage(response) {
+  readMe = `# ${response.name} 
+## Table of Contents.
+- [Description.](#description.)
 
-function createPage(response){
-readMe = `# ${response.name} 
+- [Installation instructions.](#installationinstructions.)
 
-## Description 
-${response.description}
+- [Usage information.](#usageinformation.)
 
-${response}
+- [Test instructions.](#testinstructions.)
 
+## Description. 
+${response.description}.
 
+${generateSection(response.sections)}
 
+## License.
 
-
-
+${licenseResponse(response.license)}
 
 # Contact Me / Questions.
 ### ${response.github}
@@ -71,114 +77,110 @@ ${response}
 ### ${response.email}
 `;
 
-fs.writeFile('README.md', readMe, (err) =>
-err ? console.error(err) : console.log('README created'));
-if(response.gitignore){
-fs.writeFile('.gitignore', ignore, (err) =>
-err ? console.error(err) : console.log('.gitignore added'));
-}};
-
-ignore = `node_modules`;
-
+  fs.writeFile('README.md', readMe, (err) =>
+    err ? console.error(err) : console.log('README created'));
+  if (response.gitignore) {
+    fs.writeFile('.gitignore', ignore, (err) =>
+      err ? console.error(err) : console.log('.gitignore added'));
+  }
+};
 
 
+function generateSection(response) {
+  let template = ``;
+  if (response.includes('Installation instructions')) {
+    template += `## Installation instructions.
+${section0}.
 
+`;
+  }
+  if (response.includes('Usage information')) {
+    template += `## Usage Information.
+${section1}.
 
+`;
+  }
+  if (response.includes('Contributions guidelines')) {
+    template += `## Contributions.
+${section2}. 
+
+`;
+  }
+  if (response.includes('Test instructions')) {
+    template += `## Test Instructions.
+${section3}.
+
+`;
+  }
+  return template;
+}
 
 // section choices
 
-const sectionC1 = `## Installation 
+const section0 = `
 Within a particular ecosystem, there may be a common way of installing things, such as using Yarn, NuGet, or Homebrew. However, consider the possibility that whoever is reading your README is a novice and would like more guidance. Listing specific steps helps remove ambiguity and gets people to using your project as quickly as possible. If it only runs in a specific context like a particular programming language version or operating system or has dependencies that have to be installed manually, also add a Requirements subsection.`;
 
 
-const sectionC2 = `## Usage
+const section1 = `
 Use examples liberally, and show the expected output if you can. It's helpful to have inline the smallest example of usage that you can demonstrate, while providing links to more sophisticated examples if they are too long to reasonably include in the README.`;
 
 
-const sectionC3 = `## Contributing
+const section2 = `
 State if you are open to contributions and what your requirements are for accepting them.
 
 For people who want to make changes to your project, it's helpful to have some documentation on how to get started. Perhaps there is a script that they should run or some environment variables that they need to set. Make these steps explicit. These instructions could also be useful to your future self.
 
 You can also document commands to lint the code or run tests. These steps help to ensure high code quality and reduce the likelihood that the changes inadvertently break something. Having instructions for running tests is especially helpful if it requires external setup, such as starting a Selenium server for testing in a browser.`;
 
-const sectionC4 = `## Test Instructions
+const section3 = `
 Here you can detail various methods of testing your final project.`;
 
 
-// license options
+// license options inside function returning content if selected in prompts
 
-const licenseMit = `MIT License terms--
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+function licenseResponse(license) {
+  const licenseMit = `
+  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.'
+  MIT License terms
+  
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  `;
 
-Copyright (c) <year> 
-
-## Licenses
-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-`;
-
-const licenseMpl2 = `MPL2 warranty license
+  const licenseMpl2 = `MPL2 warranty license
 [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
 
 
 Covered Software is provided under this License on an “as is” basis, without warranty of any kind, either expressed, implied, or statutory, including, without limitation, warranties that the Covered Software is free of defects, merchantable, fit for a particular purpose or non-infringing. The entire risk as to the quality and performance of the Covered Software is with You. Should any Covered Software prove defective in any respect, You (not any Contributor) assume the cost of any necessary servicing, repair, or correction. This disclaimer of warranty constitutes an essential part of this License. No use of any Covered Software is authorized under this License except under this disclaimer.`;
 
-const licensePearl =`Pearl license info
+  const licensePerl = `Pearl license info
 [![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)
 
 
 (14) Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS “AS IS’ AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES. THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE`;
-
-const licenseApache = `Apache 2.0 
+  const licenseApache = `Apache 2.0 
 [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-     http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.`;
-   
-
-
-
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
-
-
+  if (license === 'MIT') {
+    return licenseMit;
+  } else if (license === 'Mozilla') {
+    return licenseMpl2;
+  } else if (license === 'Perl') {
+    return licensePerl;
+  } else {
+    return licenseApache;
+  }
+}
